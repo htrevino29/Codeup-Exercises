c3d633757d980bfc09c0438c165d283a6e1e3fed
htrevino29 <htrevino29@gmail.com>
2016-08-29 16:55:19 -0500

    killed the do while game!

---
+++ do_while.php
@@ -0,0 +1,31 @@
+<?php 
+
+
+/// problem 1
+$i = 2;
+
+do {
+	echo $i. PHP_EOL;
+	$i+=2;
+} while ($i <=100);
+
+
+/// problem 2
+
+$b= 100;
+
+do {
+	echo $b. PHP_EOL;
+	$b= $b-5;
+} while ($b >=-10);
+
+/// problem 3
+
+$c = 2;
+
+do {
+	echo $c. PHP_EOL;
+	$c= $c * $c;
+} while ($c <=1000000);
+
+// BOOM!

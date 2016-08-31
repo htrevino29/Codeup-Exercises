2064159e1b062d0ed5e51bbc7aa21011e8ec6ed6
htrevino29 <htrevino29@gmail.com>
2016-08-31 10:59:10 -0500

    for.php complete. Banana proofed that ish.

---
+++ fizzBuzz.php
@@ -0,0 +1,12 @@
+<?php  
+
+for ($i = 0; $i <= 100; $i += 1) {
+	if($i / 3 = 1 ){
+		echo "Fizz".PHP_EOL;
+	}
+	// if($i % 5){
+	// 	echo "Buzz".PHP_EOL;
+	// } else{
+     	// echo "{$i}\n".PHP_EOL;
+	// }
+}
---
+++ for.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+
+function tryAgain(){
+	fwrite(STDOUT, 'enter a low and high value!? '.PHP_EOL);
+	fwrite(STDOUT, 'Low number first...'.PHP_EOL);
+
+	$minValue = trim(fgets(STDIN).PHP_EOL);
+	if(is_numeric($minValue)){
+		fwrite(STDOUT, 'Now the high'.PHP_EOL);
+		$maxValue = trim(fgets(STDIN).PHP_EOL);
+
+		if(is_numeric($maxValue)){
+			fwrite(STDOUT, 'enter a value to increment by'.PHP_EOL);
+			$incrementValue = trim(fgets(STDIN).PHP_EOL);
+
+			if(is_numeric($incrementValue && $incrementValue != 0)){
+				echo "/\/\+++++\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/".PHP_EOL;
+				echo "/\/\/\/\/\/\/\/\/\/\do math\/\/\/\/\/++\/\/\/".PHP_EOL;
+				echo "/\/\/\/\moar math\/\/\/\/\/\/\/\/\/\/\/\/\/\/".PHP_EOL;
+				echo "/\/\/\/\/\/\/\/\/+++/\/\/\/\/\/\ok done\/\/\/".PHP_EOL;
+			} else {
+				fwrite(STDOUT, 'you suck at life try again'.PHP_EOL);
+				tryAgain();
+			}
+
+		} else {
+			fwrite(STDOUT, 'you suck at life try again'.PHP_EOL);
+			tryAgain();
+		}
+	} else {
+		fwrite(STDOUT, 'you suck at life try again'.PHP_EOL);
+		tryAgain();
+	}
+
+	for ($minValue = 0; $minValue <= $maxValue; $minValue += $incrementValue) {
+		echo $minValue.PHP_EOL;
+	}
+	
+}	
+tryAgain();
+
+
+
+

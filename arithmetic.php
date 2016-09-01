10ba727920ecb1b3447f16115538a6bce9b863f4
htrevino29 <htrevino29@gmail.com>
2016-09-01 11:51:39 -0500

    added global variable function test.

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,12 @@
 <?php
 
+
+
+// global variables added for exercise.
+$val1 = 5;
+$val2 = 10;
+
+
 function add($a, $b)
 {
     return $a + $b;
@@ -25,3 +32,17 @@ function divide($a, $b)
 	echo divide(10,2).PHP_EOL;
 
 // Add code to test your functions here
+// Add a function modulus that finds the modulus of 2 numbers.
+// Add test code and verify the output of modulus.
+function modulus($a, $b)
+{
+	return $a % $b;
+}
+	echo modulus(9,3).PHP_EOL;
+
+
+echo "----------------------- global stuff ----------------------".PHP_EOL;
+echo add($val1,$val2).PHP_EOL;
+echo subtract($val1,$val2).PHP_EOL;
+echo multiply($val1,$val2).PHP_EOL;
+echo divide($val1,$val2).PHP_EOL;

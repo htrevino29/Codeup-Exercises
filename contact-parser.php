7229213dd94f38931e1e5ea9e1e3e578cd2ca701
htrevino29 <htrevino29@gmail.com>
2016-09-06 14:39:10 -0500

    arrays now have named indexes. working on fixing ugly phone input

--- contact-parser.php
+++ contact-parser.php
@@ -1,24 +1,30 @@
 <?php
 
-// function parseContacts($filename)
-// {
-//     $contacts = array();
+function parseContacts($filename)
+{
+    $contacts = array();
 
-//     // todo - read file and parse contacts
-//     $filename = 'contacts.txt';
-// 	$handle = fopen($filename, 'r');
-// 	$contents = fread($handle, filesize($filename));
-// 	$people = explode("\n", $contents);
-// 	foreach($people as $person){		
-// 		$info = explode("|", $person);
-// 		var_dump($info);	
-// 	}
+    // todo - read file and parse contacts
+    
+	$handle = fopen($filename, 'r');
+	$contents = fread($handle, filesize($filename));
+	$people = explode("\n", $contents);
+	array_pop($people);
+	foreach($people as $person)
+	{		
+		$info = explode("|", $person);
+		$nameArray =[];
+		$nameArray['Name'] = $info[0];
+		$nameArray['Phone'] = $info[1];
+		array_push($contacts , $nameArray);
+	}
+	
+	fclose($handle);
+    return $contacts;
+}
 
-// 	fclose($handle);
-//     return $contacts;
-// }
-
-// var_dump(parseContacts('contacts.txt'));
+$filename = 'contacts.txt';
+var_dump(parseContacts($filename));
 
 
 
@@ -54,43 +60,9 @@
 // }
 
 
-function formatPhoneNumber($numberString)
-{
-	//....
-
-}
 
 
-function parseContacts()
-{
-	// array that will hold associative arrays representing one contact
-    $contacts = [];
 
-    // get the file contents..
-    $filename = 'contacts.txt';
-	$handle = fopen($filename, 'r');
-	$contents = fread($handle, filesize($filename));
-	var_dump( $contents);
-	fclose($handle);
-   
-    //convert contents to an array of lines in the file
-
-
-    // loop through the contents array
-
-    // foreach line in the file..
-    	// construct an associative array with a 'name' and 'phone' key
-    		//we'll need to explode each line on '|' to get the name and number
-    		// do something to format the phone number, maybe formatPhoneNumber
-    		// function?
-    	// push that associavite array onto the contacts array.
-
-
-    return $contacts;
-
-}
-
-var_dump(parseContacts($filename));
 
 
 

d905f557ead3d5fdb26b79c770eb0698ae2d355d
htrevino29 <htrevino29@gmail.com>
2016-09-13 15:57:37 -0500

    fers answer to the construct & deconstruct excercise. wooh!

---
+++ fers_lesson.php
@@ -0,0 +1,37 @@
+<!-- fer is killing the log game. his attempt to solve the lesson. -->
+<?php  
+
+class Log
+{
+	public $filename;
+	public $datetime;
+	public $handle;
+
+	function __construct($prefix = 'log')
+	{
+		$this->datetime = date("Y-m-d");
+		$this->filename = "$prefix-{$this->datetime}.log";
+		$this->handle = fopen($this-filename, "a");
+
+	}
+	function __destruct()
+	{
+		fclose($this->handle);
+	}
+	function logMessage($loveLevel, $message)
+	{
+		$string = "{$this->datetime} $logLevel $message".PHP_EOL;
+		fwrite($this->handle,$string);
+	}
+	function info()
+	{
+		$this->logMessage("INFO", "This is the first info message.");
+	}
+	function error()
+	{
+		$this->logMessage("ERROR", "This is the second info message.");
+	}
+
+}
+
+?>
